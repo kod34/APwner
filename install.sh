6cd3fd2f0a415d732663066e53d7f4f04d6f74f8
kod3a <5od3a.yes@protonmail.com>
2022-04-25 10:07:04 -0400

    Update1.2

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt install aircrack-ng macchanger xterm python3-pip -y
 sudo pip3 install psutil prettytable
-sudo ln -s $(readlink -f codice.py) ${PATH%%:*}/apwner
+sudo ln -s $(readlink -f apwner.py) ${PATH%%:*}/apwner
